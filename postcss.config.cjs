6e8b894eff4862e1d9e803aeb5416555a0e978e7
Vítor Arimatéa <vitorodev@outlook.com>
2023-01-30 16:47:08 -0300

    First commit

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+  return (
+    <div>
+      <h1>Olá, mundo! :D</h1>
+      <p>Seja muito bem vindo ao meu site!</p>
+    </div>
+  )
+}
+
+export default App
